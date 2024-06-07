839ca451360f9447ccb893f178dfc95c24eddaf4
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-06-08 01:15:25 +0300

    Edit Post UI, add simple query and api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -252,3 +252,20 @@ export async function deleteSavedPost(savedRecordId: string) {
                 console.log(error);
         }
 }
+
+// ============================== GET POST BY ID
+export async function getPostById(postId?: string) {
+        if (!postId) throw Error;
+        try {
+                const post = await databases.getDocument(
+                        appwriteConfig.databaseId,
+                        appwriteConfig.postCollectionId,
+                        postId,
+                );
+
+                if (!post) throw Error;
+                return post;
+        } catch (error) {
+                console.log(error);
+        }
+}
