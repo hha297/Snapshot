789db4d30743563ccd8c4e23df6b5bd05430b3eb
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-06-11 20:39:17 +0300

    Post details

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -136,7 +136,11 @@ const PostForm = ({ post, action }: PostFormProps) => {
                                         )}
                                 />
                                 <div className="flex gap-4 items-center justify-end">
-                                        <Button type="button" className="shad-button_dark_4">
+                                        <Button
+                                                type="button"
+                                                className="shad-button_dark_4"
+                                                onClick={() => navigate(-1)}
+                                        >
                                                 <p className="text-red">Cancel</p>
                                         </Button>
                                         <Button
