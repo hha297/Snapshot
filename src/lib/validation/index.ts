d97d1e092bb4f3091fd52d29e3dfff00d086041c
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-06-13 22:51:54 +0300

    update profile

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -12,9 +12,27 @@ export const SignInValidation = z.object({
         password: z.string().min(8, { message: 'Password must contain at least 8 character(s)' }),
 });
 
-export const PostValidation = z.object({
-        caption: z.string().min(5).max(2200),
+export const ProfileValidation = z.object({
         file: z.custom<File[]>(),
-        location: z.string().min(2).max(255),
+        name: z.string().min(2, { message: 'Name must be at least 2 characters.' }),
+        username: z.string().min(2, { message: 'Name must be at least 2 characters.' }),
+        email: z.string().email(),
+        bio: z.string(),
+});
+
+// ============================================================
+// POST
+// ============================================================
+
+export const PostValidation = z.object({
+        caption: z
+                .string()
+                .min(5, { message: 'Minimum 5 characters.' })
+                .max(2200, { message: 'Maximum 2,200 caracters' }),
+        file: z.custom<File[]>(),
+        location: z
+                .string()
+                .min(1, { message: 'This field is required' })
+                .max(1000, { message: 'Maximum 1000 characters.' }),
         tags: z.string(),
 });
